4c6819d8b39f2ea2a9cda477ff3fb36581779887
safwanpaleri <safwanpaleri@gmail.com>
2021-11-13 12:34:00 +0530

    Create PlayerShipMovement.cs

--- PlayerShipMovement.cs
+++ PlayerShipMovement.cs
@@ -1,10 +1,12 @@
-﻿using System;
+//Unity Headers
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerShipMovement : MonoBehaviour
 {
+    //creating variables.
     [SerializeField] float XSpeed = 10f;
     [SerializeField] float YSpeed = 10f;
    
@@ -13,40 +15,53 @@ public class PlayerShipMovement : MonoBehaviour
 
     [SerializeField] float Yrotate = 5f;
     [SerializeField] float Zcontroll = -70f;
-
+    
+    //multiple guns can be attached, caching them in an array
+    // for easiness in using them.
     [SerializeField] GameObject[] Guns;
 
     float HorizontalThrow;
     float VerticalThrow;
 
     bool Alive = true;
-
+    
     void Update()
     {
+        //while the shp is alive
         if (Alive)
         {
+            //functions for controlling the movements
             ShipMovement();
             ShipRotation();
+            //function for shooting
             ShootController();
         }
     }
+    
+    //function for destroying the ship
    public void DestroyShip()
     {
         Alive = false;
     }
+    
+    //function for ship movement
     private void ShipMovement()
     {
+        //contolling horizontal ovement
         HorizontalThrow = Input.GetAxis("Horizontal") * XSpeed * Time.deltaTime;
         float NewXpos = transform.localPosition.x + HorizontalThrow;
-
+        
+        //controlling vertical movement
         VerticalThrow = Input.GetAxis("Vertical") * YSpeed * Time.deltaTime;
         float NewYpos = transform.localPosition.y + VerticalThrow;
-
+        
         transform.localPosition = new Vector3(Mathf.Clamp(NewXpos, -5f, 5f), Mathf.Clamp(NewYpos, -3.5f, 3.5f), transform.localPosition.z);
     }
-
+    
+    //function for controlling ship rotation
     private void ShipRotation()
     {
+        //controlling rotation
         float Pitch = transform.localPosition.y * Xrotate + VerticalThrow * Xcontroll;
         float Raw = transform.localPosition.x * Yrotate;
         float Roll = HorizontalThrow * Zcontroll;
@@ -54,24 +69,29 @@ public class PlayerShipMovement : MonoBehaviour
         transform.localRotation = Quaternion.Euler(Pitch, Raw, Roll);
     }
 
+    //function responsible for shooting
     void ShootController()
     {
+        //while the button is being pressed, fire continously.
         if(Input.GetButton("Fire1"))
         {
-            FireInTheHole(true);
+            Fire(true);
         }
         else
         {
-            FireInTheHole(false);
+            Fire(false);
         }
     }
-
-    private void FireInTheHole(bool confirmation)
+    
+    //function responsible for firing shots
+    private void Fire(bool isTrue)
     {
+        //multiple guns can be attached, and do the firing for all of them
         foreach (GameObject gun in Guns)
-        {
+        {   
+            //fire the shots.
             var EmissionModule = gun.GetComponent<ParticleSystem>().emission;
-            EmissionModule.enabled = confirmation;
+            EmissionModule.enabled = isTrue;
         }
     }
 }
